4dde535f3e83ca7e28c4103c96470619b8a5c5c6
R0yJing <roy.jing@outlook.com>
2021-07-19 15:24:18 +1200

    changed App.js

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -10,7 +10,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-links"
+
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
